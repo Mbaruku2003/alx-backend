ad4642b9596afc9716f0d3c8ae52df7c437b13c8
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-29 10:56:48 +0300

    created a new kwy

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""a catching system with no limit"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """You must use self.cache_data"""
+
+    def put(self, key, item):
+        """Assign to the dictionary the value of the key"""
+        
+        if key is not None and item is not None:
+            self.cache_data[Key] = item
+
+    def get(self, key):
+        """Return the value in self.cache_daya linked to key"""
+
+        return self.cache_data.get(key, None)
