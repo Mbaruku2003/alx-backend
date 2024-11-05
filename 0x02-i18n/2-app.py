5a4bde95461580098b01e7675042843d9b15e2cb
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-05 19:27:45 +0300

    tcreated new file

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" basic flask app."""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """returns an expected language that fits the best."""
+
+    return request.accept_languages.best_match(["en"])
+
+@app.route('/')
+def index():
+    """Render template."""
+
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,2 @@
+we will deal with localalisation and internalisation Li0n and il8n respectively
+this allows us to make programmes accessible in different languages
