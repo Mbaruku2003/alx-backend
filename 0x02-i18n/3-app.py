cc51897cb45ceb388613128fd5ea3bb9c93ba096
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-05 21:05:15 +0300

    created a new file

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 import typing
 
+
 app = Flask(__name__)
 
 
@@ -26,6 +27,7 @@ def get_locale() -> str:
     """returns an expected language that fits the best."""
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index() -> str:
