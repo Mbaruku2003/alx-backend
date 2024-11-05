757b21fbfe7f5b350b23ee25d4acddad87554859
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-05 20:56:05 +0300

    created a new file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-""" basic flask app."""
-from flask import Flask, render_template
-import typing
+'''Task 0: Basic Flask app
+'''
 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index() -> str:
-    """Render template."""
-
-    return render_template('0-index.html')
+def index():
+    '''default route'''
+    return render_template("0-index.html",)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
