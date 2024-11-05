caa8d1a76baa51a35248d199e47e54d9464aa560
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-05 22:12:41 +0300

    created a new file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     '''default route'''
-    return render_template("0-index.html",)
+
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
