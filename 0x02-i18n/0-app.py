1cc20225d05f7be0e515f925a924e040f6fefa66
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-05 19:34:02 +0300

    created file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,13 +3,16 @@
 from flask import Flask, render_template
 import typing
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index() -> str:
     """Render template."""
 
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
